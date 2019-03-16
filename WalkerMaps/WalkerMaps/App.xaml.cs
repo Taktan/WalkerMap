ad1a97a40b49382fbc1ab9ca5341d0eca58447b0
SotrydnikSee <sotrydnik.see@gmail.com>
2019-03-16 12:27:20 +0400

    Сделан загрузочный экран

--- WalkerMaps/WalkerMaps/App.xaml.cs
+++ WalkerMaps/WalkerMaps/App.xaml.cs
@@ -11,7 +11,7 @@ namespace WalkerMaps
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new splashPage();
         }
 
         protected override void OnStart()
